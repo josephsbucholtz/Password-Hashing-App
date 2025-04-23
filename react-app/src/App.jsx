3ab91f60b4818bf50cba0c0c9c4409e8c785a9c9
josephsbucholtz <joseph.bucholtz@sjsu.edu>
2025-04-23 01:31:46 -0700

    More style

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import Login from './components/Login'
 import Signup from './components/SignUp'
 
 function App() {
   return (
-    <div>
-      <Signup/>
+    <div className="items-center">
+      <div className="flex items-center justify-center p-20">
+        <Signup/>
+      </div>
+      <div className="flex items-center justify-center">
+        <Login />
+      </div> 
     </div>  
   )
 }
--- react-app/src/components/Login.jsx
+++ react-app/src/components/Login.jsx
@@ -1,9 +1,9 @@
 
-const Signup = () => {
+const Login = () => {
   return (
     <div>
         <div className="bg-slate-800 border border-slate-600 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-lg bg-opacity-30 relative">
-            <h1 className="font-bold text-center text-4xl">Sign Up</h1>
+            <h1 className="font-bold text-center text-4xl">Login</h1>
             <form action="">
                 <div className="my-4 relative">
                     <input type="text" className="block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 
@@ -15,11 +15,11 @@ const Signup = () => {
                     appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer" placeholder="Password"></input>
                     <label htmlFor="" className="absolute text-sm duration-300 transform -translate scale-75 top-3 -z-10 origin-[0]"></label>
                 </div>
-                <button type="submit" className="w-full mb-4 text-[18px] roundedbg-blue-500 py-2 hover:bg-blue-600 transition-colors duration-300">Sign Up</button>
+                <button type="submit" className="">Login</button>
             </form>
         </div>
     </div>
   )
 }
 
-export default Signup
+export default Login
