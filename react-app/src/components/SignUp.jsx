aaa2eb152c2fb8019a03b7981751abcab164c0c3
josephsbucholtz <joseph.bucholtz@sjsu.edu>
2025-04-23 00:52:58 -0700

    Initial commit

---
+++ react-app/src/components/SignUp.jsx
@@ -0,0 +1,13 @@
+
+const Signup = () => {
+  return (
+    <div>
+        <div>
+            <h1 className="text-8x1 font-bold text-center">Sign Up</h1>
+            <form action=""></form>
+        </div>
+    </div>
+  )
+}
+
+export default Signup
