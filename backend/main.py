2f26ecfa49f6d083e2f60ab9ac4c2eafc67733f2
josephsbucholtz <joseph.bucholtz@sjsu.edu>
2025-04-25 16:35:23 -0700

    JSON File Fix

--- backend/main.py
+++ backend/main.py
@@ -16,22 +16,30 @@ def signup():
     username = data.get('username')
     password = data.get('password')
 
-    #Bcrypt Hashing Algorithm
-    bytes = password.encode('utf-8')
-
-    salt = bcrypt.gensalt()
-
-    password_hash = bcrypt.hashpw(bytes, salt)
-
     if not username or not password:
         print('Missing username or password')
         return jsonify({'error': 'Missing data'}), 400
 
+    #Bcrypt Hashing Algorithm
+    bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    password_hash = bcrypt.hashpw(bytes, salt)
+
     user_data = {'username': username, 'password': password_hash.decode("utf-8")}
 
+    users = []
+
+    # If the file exists and is not empty, load existing users
+    if os.path.exists('users.json') and os.path.getsize('users.json') > 0:
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+
+    users.append(user_data)
+
+    #Users to JSON
     try:
-        with open('users.json', 'a') as f:
-            f.write(json.dumps(user_data) + '\n')
+        with open('users.json', 'w') as f:
+            json.dump(users, f, indent=4)
     except Exception as e:
         print('File write error ❌:', e)
         return jsonify({'error': 'Failed to write file'}), 500
